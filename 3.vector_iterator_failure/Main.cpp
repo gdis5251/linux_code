abdfeeaa53a253bc23bb05b1657bba8bc28ff8f5
gdis5251 <841784596@qq.com>
2019-04-01 17:22:34 +0800

    [Gerald][update][2019/4/1] Finally I figure out what's going on, There was a capacity expansion just now

--- 3.vector_iterator_failure/Main.cpp
+++ 3.vector_iterator_failure/Main.cpp
@@ -3,11 +3,22 @@
 
 int main(void)
 {
-    int arr[] = { 1, 2, 3, 4 };
-    std::vector<int> v1(arr, arr + sizeof(arr) / sizeof(int));
+    std::vector<int> v1;
+    v1.push_back(1);
+    v1.push_back(2);
+    v1.push_back(3);
+    v1.push_back(4);
+    v1.push_back(5);
     
     std::vector<int>::iterator it_begin = v1.begin();
     std::vector<int>::iterator it_end = v1.end() - 1;
+    while (it_begin <= it_end)
+    {
+        std::cout << *it_begin << " ";
+        it_begin++;
+    }
+    std::cout << std::endl;
+    it_begin = v1.begin();
     v1.insert(v1.begin() + 1, 10);
 
     std::cout << *it_begin << std::endl;
