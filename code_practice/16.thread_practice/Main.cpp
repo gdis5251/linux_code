372b9f9884674651688f7a61fbcb573b1a106f44
gdis5251 <841784596@qq.com>
2019-06-01 17:32:49 +0800

    [Gerald][write][2019/6/1] 完成对线程部分代码的基本练习

---
+++ code_practice/16.thread_practice/Main.cpp
@@ -0,0 +1,238 @@
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+
+// 创建线程
+#if 0
+void *EntryFunc(void *arg)
+{
+    (void) arg;
+
+    while (true)
+    {
+        std::cout << "I am thread 1 !" << std::endl;
+        sleep(1);
+    }
+
+    return nullptr;
+}
+
+int main(void)
+{
+    // 线程 id
+    pthread_t tid;
+    int ret;
+
+    if ((ret = pthread_create(&tid, nullptr, EntryFunc, nullptr)) != 0)
+    {
+        perror("pthread_create");
+        exit(EXIT_FAILURE);
+    }
+
+    // 走到这里说明线程创建成功
+    while (true)
+    {
+        std::cout << "I am Main thread !" << std::endl;
+        sleep(1);
+    }
+
+    return 0;
+}
+#endif
+
+// 线程id
+#if 0
+void *EntryFunc(void *arg)
+{
+    (void)arg;
+    *(pthread_t *)arg = pthread_self();
+
+    std::cout << "Thread 1 : " << *(pthread_t*)arg << std::endl;
+    return nullptr;
+}
+
+
+int main(void)
+{
+    pthread_t tid;
+    pthread_t tid_1;
+    pthread_create(&tid, NULL, EntryFunc, &tid_1);
+    std::cout << "Main Thread:  " << tid << std::endl;
+
+    while (true);
+    return 0;
+}
+#endif
+
+// 进程终止
+// pthread_exit
+#if 0
+void *EntryFunc(void *arg)
+{
+    (void) arg;
+
+    std::cout << "I am thread 1 !" << std::endl;
+
+    pthread_exit(nullptr);
+
+    std::cout << "Test pthread_exit ." << std::endl;
+
+    return nullptr;
+}
+
+int main(void)
+{
+    pthread_t tid;
+    pthread_create(&tid, nullptr, EntryFunc, nullptr);
+
+    while (true)
+    {
+        sleep(1);
+    }
+    return 0;
+}
+#endif
+
+// pthread_cancel
+#if 0
+
+std::vector<int> arr(1000000);
+
+void *EntryFunc(void *arg)
+{
+    (void) arg;
+
+    std::cout << "Assignment Start! " << std::endl;
+    for(size_t i = 0; i < arr.size(); i++)
+    {
+        arr[i] = i;
+    }
+    std::cout << "Assignment Done! " << std::endl;
+
+    return nullptr;
+}
+
+int main(void)
+{
+    pthread_t tid;
+    pthread_create(&tid, nullptr, EntryFunc, nullptr);
+
+    sleep(1);
+    pthread_cancel(tid);
+    while (true)
+    {
+        sleep(1);
+    }
+    return 0;
+}
+#endif
+
+#if 0
+
+void *ThreadEntry(void *arg)
+{
+    (void) arg;
+
+    sleep(5);
+
+    std::cout << "Thread 1 Exit" << std::endl;
+
+    return nullptr;
+}
+
+int main(void)
+{
+    pthread_t tid;
+    pthread_create(&tid, nullptr, ThreadEntry, nullptr);
+
+    // 不回收线程
+    while (true)
+    {
+        sleep(1);
+    }
+
+    return 0;
+}
+#endif
+
+
+// pthread_join 
+#if 0
+void *ThreadEntry(void *arg)
+{
+    (void) arg;
+
+    std::cout << "Thread 1." << std::endl;
+    while (true);
+
+    return nullptr;
+}
+
+int main(void)
+{
+    pthread_t tid;
+    pthread_create(&tid, nullptr, ThreadEntry, nullptr);
+ 
+    std::cout << "Main Thread." << std::endl;
+    pthread_join(tid, nullptr);
+
+    return 0;
+}
+#endif
+
+#if 0
+void *ThreadEntry(void *arg)
+{
+    while (true)
+    {
+        std::cout << (char *)arg << std::endl;
+        sleep(1);
+    }
+}
+
+int main(void)
+{
+    pthread_t tid;
+    pthread_create(&tid, nullptr, ThreadEntry, (void*)"Thread running...");
+    
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << "Main Thread runing...." << std::endl;
+        sleep(1);
+    } 
+    exit(1);
+
+    return 0;
+}
+#endif
+
+// 使用gdb调试多线程程序
+#if 1
+void *ThreadEntry(void *arg)
+{
+    (void)arg;
+
+    while (true)
+    {
+        std::cout << "Thread 1 runnning ... " << std::endl;
+        sleep(1);
+    }
+
+    return nullptr ;
+}
+
+int main(void)
+{
+    pthread_t tid;
+    pthread_create(&tid, nullptr, ThreadEntry, nullptr);
+
+    std::cout << "Main Thread running ...." << std::endl;
+
+    pthread_join(tid, nullptr);
+
+    return 0;
+}
+#endif
